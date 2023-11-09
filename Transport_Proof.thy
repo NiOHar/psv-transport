5e51dfdaa2c58397adb78cc0bfb0cbd3b959bf7d
Nils Harmsen <harmsen@cs.tum.edu>
2023-11-09 15:52:46 +0100

    generalised lemmata

--- Transport_Proof.thy
+++ Transport_Proof.thy
@@ -26,30 +26,43 @@ lemma bi_unique_onD2:
   shows "x = x'"
   using assms unfolding bi_unique_on_def using rel_injective_onD by auto
 
+abbreviation "rel_bi_unique T \<equiv> bi_unique_on (\<top> :: 'a \<Rightarrow> bool) (T :: 'a \<Rightarrow> 'b \<Rightarrow> bool)"
 
-theorem bi_unique_imp_rel_eq:
-  assumes biuni: "bi_unique_on (\<top>::'a \<Rightarrow> bool) (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
-  shows "(T \<Rrightarrow> T \<Rrightarrow> (=)) (=) (=)"
+
+
+theorem bi_unique_imp_rel_eq_on_pred:
+  assumes "bi_unique_on (P :: 'a \<Rightarrow> bool) (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
+  and "(T \<Rrightarrow> (\<longleftrightarrow>)) P Q"
+  shows "(T \<Rrightarrow> T \<Rrightarrow> (\<longleftrightarrow>)) (eq_onp P) (eq_onp Q)"
 proof (intro Dep_Fun_Rel_relI)
   fix a a' b b'
-  assume taa: "T a a'" and tbb: "T b b'"
-  show "(a = b) = (a' = b')" proof (cases "a = b")
-    case True
-    then have "T a b'" using tbb by blast
-    then have "a' = b'" using biuni taa bi_unique_onD1[of \<top> T a a' b'] by blast
-    then show ?thesis using True by presburger
+  assume "T a a'" and "T b b'"
+  with assms show "(eq_onp P a b) \<longleftrightarrow> (eq_onp Q a' b')" proof (auto, goal_cases)
+    case 1              
+    then have "P a \<Longrightarrow> Q a'" using assms by blast
+    from 1 have "T a b'" using \<open>T b b'\<close> eq_onp_to_eq by fastforce
+    then have "a' = b'" using assms \<open>T a a'\<close> \<open>eq_onp P a b\<close> eq_onp_def
+      by (metis bi_unique_onD1)
+    then show ?case
+      by (metis "1"(5) \<open>P a \<Longrightarrow> Q a'\<close> eq_onp_def)
   next
-    case False
-    show ?thesis proof (rule ccontr)
-      assume "(a = b) \<noteq> (a' = b')"
-      from this False have "a' = b'" by argo
-      then have "a = b" using biuni bi_unique_onD2[of \<top> T a b b']  taa tbb by blast
-      from this False show "False" by argo
-    qed
- qed
+    case 2
+    then show ?case
+      by (smt (z3) Dep_Fun_Rel_rel_def bi_unique_onD2 eq_onp_def)
+  qed
 qed
 
-definition "bi_total_on A B R = (rel_surjective_at B R \<and> left_total_on A R)"
+theorem bi_unique_imp_rel_eq:
+  assumes "rel_bi_unique (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
+  shows "(T \<Rrightarrow> T \<Rrightarrow> (\<longleftrightarrow>)) (=) (=)"
+proof (intro Dep_Fun_Rel_relI)
+  fix a a' b b'
+  assume "T a a'" and "T b b'"
+  with assms show "(a = b) \<longleftrightarrow> (a' = b')" by (auto dest: bi_unique_onD1 bi_unique_onD2)
+ qed
+
+
+definition "bi_total_on P Q R = (left_total_on P R \<and> rel_surjective_at Q R)"
                                                                
 lemma bi_total_onI [intro]:
   assumes "\<And>y. Q y \<Longrightarrow> in_codom R y"
@@ -71,30 +84,35 @@ obtains "y" where "R x y"
   unfolding bi_total_on_def using left_total_onE assms
   by (metis bi_total_on_def)
 
-theorem bi_total_imp_rel_all: assumes bit: "bi_total_on (\<top>::'a \<Rightarrow> bool) (\<top>::'b \<Rightarrow> bool) (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
-  shows "((T \<Rrightarrow> (=)) \<Rrightarrow> (=)) All All"
+abbreviation "rel_bi_total T \<equiv> bi_total_on (\<top> :: 'a \<Rightarrow> bool) (\<top> :: 'b \<Rightarrow> bool) (T :: 'a \<Rightarrow> 'b \<Rightarrow> bool)"
+
+
+theorem bi_total_on_imp_rel_all_on: assumes "bi_total_on (P :: 'a \<Rightarrow> bool) (Q :: 'b \<Rightarrow> bool) (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
+  and "(T \<Rrightarrow> (\<longleftrightarrow>)) P Q"
+  shows "((T \<Rrightarrow> (\<longleftrightarrow>)) \<Rrightarrow> (\<longleftrightarrow>)) (\<lambda> p . (\<forall> x . P x \<longrightarrow> p x)) (\<lambda> q . (\<forall> x . Q x \<longrightarrow> q x))"
 proof (intro Dep_Fun_Rel_relI)
-  fix a b
-  assume reled: "(T \<Rrightarrow> (=)) (a::'a \<Rightarrow> bool) (b::'b \<Rightarrow> bool)"
-  then show "(All a) = (All b)" proof (cases "All a")
-    case True
-    then have "All b" proof
-      fix y
-      obtain "x" where "T x y" using bi_total_onE1[of \<top> \<top> T y] bit by blast
-      from this True have "a x" by blast
-      then have "b y" using \<open>T x y\<close> reled by blast
-    then show "All b"
-      by (metis (full_types, lifting) Dep_Fun_Rel_relD True bi_total_onE1 bit reled top_conj(2))
+  fix p q
+  assume reled: "(T \<Rrightarrow> (\<longleftrightarrow>)) (p::'a \<Rightarrow> bool) (q::'b \<Rightarrow> bool)"
+  with assms show "(\<forall> x . P x \<longrightarrow> p x) \<longleftrightarrow> (\<forall> x . Q x \<longrightarrow> q x)" by (auto elim: bi_total_onE1 bi_total_onE2)
+qed
+
+
+theorem bi_total_imp_rel_all: assumes "rel_bi_total (T::'a \<Rightarrow> 'b \<Rightarrow> bool)" 
+  shows "((T \<Rrightarrow> (\<longleftrightarrow>)) \<Rrightarrow> (\<longleftrightarrow>)) All All"
+proof (intro Dep_Fun_Rel_relI)
+  fix p q
+  assume reled: "(T \<Rrightarrow> (\<longleftrightarrow>)) (p::'a \<Rightarrow> bool) (q::'b \<Rightarrow> bool)"
+  with assms show "(All p) \<longleftrightarrow> (All q)" by (auto elim: bi_total_onE1 bi_total_onE2)
+qed
+
+(*
+theorem left_total_imp_Ex: assumes "(\<exists> x . (p x \<and> (\<exists> y . T x y))) \<and> (\<exists> y . (q y \<and> (\<exists> x . T x y)))"
+  shows "((T \<Rrightarrow> (\<longleftrightarrow>)) \<Rrightarrow> (\<longleftrightarrow>)) Ex Ex"
+proof (intro Dep_Fun_Rel_relI)
+  fix p q
+  assume "(T \<Rrightarrow> (\<longleftrightarrow>)) (p::'a \<Rightarrow> bool) (q::'b \<Rightarrow> bool)"
+  with assms show "(\<exists> x . p x) \<longleftrightarrow> (\<exists> x . q x)" apply (auto simp add: left_total_eq_left_total_on)
   qed
-  thus ?thesis using True by argo
-  next
-    case False
-    then obtain "x" where "\<not> a x" by blast
-    then obtain "y" where "T x y" using bi_total_onE2[of \<top> \<top> T x] bit by blast
-    from \<open>~ a x\<close> this reled have "\<not> b y" by blast
-    then have "\<not> All b" by blast
-    then show ?thesis using False by blast
-  qed
-  qed
+*)
 
 end
